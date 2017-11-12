7efa149e8c3bfc4648f1da34b1d08ea15abbef09
Matt Kromer <mkromer@cse.fraunhofer.org>
2017-11-11 22:44:38 -0500

    general clean up of legacy code

--- recompile_ess.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-volttron-ctl stop --tag ess
-volttron-ctl remove --tag ess
-volttron-pkg package ./services/contrib/ESSAgent
-volttron-ctl install --tag ess ~/.volttron/packaged/essagent-1.0-py2-none-any.whl
-#volttron-ctl config list   
-volttron-ctl start --tag ess
-volttron-ctl status
